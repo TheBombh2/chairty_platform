a2671d757c23a9baba6c8e909621bd6cc4e2946c
BelalSalem <62998842+TheBombh2@users.noreply.github.com>
2024-09-28 14:13:16 +0300

    added scaffold widget

--- lib/screens/patient_request_form.dart
+++ lib/screens/patient_request_form.dart
@@ -20,149 +20,151 @@ class RequestAndEditScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 30),
-      child: Scaffold(
-          body: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "Your Request",
-                style: TextStyle(
-                    color: darkColor,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold
-                ),
-              ),
-              const SizedBox(height: 10,),
-              Expanded(
-                child: Container(
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                      color: lightColor,
-                      borderRadius: BorderRadius.circular(20)
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 30),
+        child: Scaffold(
+            body: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Your Request",
+                  style: TextStyle(
+                      color: darkColor,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold
                   ),
-                  child:  Form(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10.0,vertical: 20),
-                      child: SingleChildScrollView(
-                        child: Column(
-                          mainAxisSize: MainAxisSize.max,
-                          children: [
-                            FormFields(
-                                controller:idController ,
+                ),
+                const SizedBox(height: 10,),
+                Expanded(
+                  child: Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        color: lightColor,
+                        borderRadius: BorderRadius.circular(20)
+                    ),
+                    child:  Form(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10.0,vertical: 20),
+                        child: SingleChildScrollView(
+                          child: Column(
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              FormFields(
+                                  controller:idController ,
+                                  type: TextInputType.number,
+                                  hint:  "Owner ID",
+                                  label:  "Owner ID",
+                                  icon: Icons.person),
+                              const SizedBox(height: 20,),
+                              FormFields(
+                                  controller: reasonController,
+                                  type:  TextInputType.multiline,
+                                  maxLines: 6,
+                                  hint:  "Reason",
+                                  label:  "Your Reason",
+                                  icon: Icons.question_mark_outlined),
+                              const SizedBox(height: 20,),
+                              FormFields(
+                                  controller:dangerController ,
+                                  type:   TextInputType.multiline,
+                                  maxLines: 6,
+                                  hint:  "Danger",
+                                  label:  "Danger",
+                                  icon: Icons.dangerous),
+                              const SizedBox(height: 20,),
+                              FormFields(
+                                controller: fundsController,
                                 type: TextInputType.number,
-                                hint:  "Owner ID",
-                                label:  "Owner ID",
-                                icon: Icons.person),
-                            const SizedBox(height: 20,),
-                            FormFields(
-                                controller: reasonController,
-                                type:  TextInputType.multiline,
-                                maxLines: 6,
-                                hint:  "Reason",
-                                label:  "Your Reason",
-                                icon: Icons.question_mark_outlined),
-                            const SizedBox(height: 20,),
-                            FormFields(
-                                controller:dangerController ,
-                                type:   TextInputType.multiline,
-                                maxLines: 6,
-                                hint:  "Danger",
-                                label:  "Danger",
-                                icon: Icons.dangerous),
-                            const SizedBox(height: 20,),
-                            FormFields(
-                              controller: fundsController,
-                              type: TextInputType.number,
-                              hint:  "Funds",
-                              label:  "Funds",
-                              icon: Icons.money, ),
-                            const SizedBox(height: 20,),
-                            FormFields(
-                              controller: docsController,
-                              type: TextInputType.text,
-                              hint:  "DOCs",
-                              label:  "Your Medical DOCs Link",
-                              icon: Icons.picture_as_pdf, ),
-                            const SizedBox(height: 20,),
-                            FormFields(
-                              controller: hospitalController,
-                              type: TextInputType.text,
-                              hint:  "Hospital",
-                              label:  "Hospital Name",
-                              icon: Icons.location_city, ),
-                            const SizedBox(height: 20,),
-                            FormFields(
-                              controller: locationController,
-                              type: TextInputType.text,
-                              hint:  "Location",
-                              label:  "Hospital Location",
-                              icon: Icons.location_on_rounded, ),
-
-                            const SizedBox(height: 20,),
-                            FormFields(
-                              controller: deadLineController,
-                              type: TextInputType.datetime,
-                              hint:  "DeadLine",
-                              label:  "DeadLine",
-                              icon: Icons.date_range,
-                              onTap: () async {
-                                DateTime? pickedDate = await showDatePicker(
-                                  context: context,
-                                  initialDate: DateTime.now(),
-                                  firstDate: DateTime.now(),
-                                  lastDate: DateTime.parse('2030-10-25'),
-                                  builder: (BuildContext context, Widget? child) {
-                                    return Theme(
-                                      data: ThemeData.light().copyWith(
-                                        primaryColor: darkColor, // Color for selected dates
-                                        colorScheme: ColorScheme.light(
-                                          primary: deepOrange, // Header background color
-                                          onPrimary: Colors.white, // Header text color
-                                          onSurface: darkColor, // Body text color
+                                hint:  "Funds",
+                                label:  "Funds",
+                                icon: Icons.money, ),
+                              const SizedBox(height: 20,),
+                              FormFields(
+                                controller: docsController,
+                                type: TextInputType.text,
+                                hint:  "DOCs",
+                                label:  "Your Medical DOCs Link",
+                                icon: Icons.picture_as_pdf, ),
+                              const SizedBox(height: 20,),
+                              FormFields(
+                                controller: hospitalController,
+                                type: TextInputType.text,
+                                hint:  "Hospital",
+                                label:  "Hospital Name",
+                                icon: Icons.location_city, ),
+                              const SizedBox(height: 20,),
+                              FormFields(
+                                controller: locationController,
+                                type: TextInputType.text,
+                                hint:  "Location",
+                                label:  "Hospital Location",
+                                icon: Icons.location_on_rounded, ),
+      
+                              const SizedBox(height: 20,),
+                              FormFields(
+                                controller: deadLineController,
+                                type: TextInputType.datetime,
+                                hint:  "DeadLine",
+                                label:  "DeadLine",
+                                icon: Icons.date_range,
+                                onTap: () async {
+                                  DateTime? pickedDate = await showDatePicker(
+                                    context: context,
+                                    initialDate: DateTime.now(),
+                                    firstDate: DateTime.now(),
+                                    lastDate: DateTime.parse('2030-10-25'),
+                                    builder: (BuildContext context, Widget? child) {
+                                      return Theme(
+                                        data: ThemeData.light().copyWith(
+                                          primaryColor: darkColor, // Color for selected dates
+                                          colorScheme: ColorScheme.light(
+                                            primary: deepOrange, // Header background color
+                                            onPrimary: Colors.white, // Header text color
+                                            onSurface: darkColor, // Body text color
+                                          ),
+                                          dialogBackgroundColor: Colors.blueGrey[50], // Background color of the picker
                                         ),
-                                        dialogBackgroundColor: Colors.blueGrey[50], // Background color of the picker
+                                        child: child!,
+                                      );
+                                    },
+                                  );
+                                  if (pickedDate != null) {
+                                    deadLineController.text = DateFormat.yMMMd().format(pickedDate);
+                                    print(DateFormat.yMMMd().format(pickedDate));
+                                  }
+                                },
+                              ),
+                              const SizedBox(height: 50,),
+                              Row(
+                                mainAxisSize: MainAxisSize.max,
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  ElevatedButton(
+                                    onPressed: (){},
+                                    child: const Text(
+                                      "Submit",
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold
                                       ),
-                                      child: child!,
-                                    );
-                                  },
-                                );
-                                if (pickedDate != null) {
-                                  deadLineController.text = DateFormat.yMMMd().format(pickedDate);
-                                  print(DateFormat.yMMMd().format(pickedDate));
-                                }
-                              },
-                            ),
-                            const SizedBox(height: 50,),
-                            Row(
-                              mainAxisSize: MainAxisSize.max,
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                ElevatedButton(
-                                  onPressed: (){},
-                                  child: const Text(
-                                    "Submit",
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.bold
+                                    ),
+                                    style: ElevatedButton.styleFrom(
+                                        backgroundColor: darkColor
                                     ),
                                   ),
-                                  style: ElevatedButton.styleFrom(
-                                      backgroundColor: darkColor
-                                  ),
-                                ),
-                              ],
-                            )
-                          ],
+                                ],
+                              )
+                            ],
+                          ),
                         ),
                       ),
                     ),
                   ),
                 ),
-              ),
-            ],
-          )
+              ],
+            )
+        ),
       ),
     );
   }
