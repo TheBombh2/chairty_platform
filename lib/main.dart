1ce3d44390cd7af4702df87cb2fbca5c4ec5d4c8
BelalSalem <62998842+TheBombh2@users.noreply.github.com>
2024-09-30 21:39:45 +0300

    firebase core & sdk installed

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:chairty_platform/screens/donator_home_screen.dart';
 import 'package:chairty_platform/screens/patient_request_form.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -14,7 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: RequestAndEditScreen(),
+      home: DonatorHomeScreen(),
     );
   }
 }
